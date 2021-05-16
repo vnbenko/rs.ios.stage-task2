e743b6ad0903c9e198e61a19580178fbc152ef7b
Vladimir Benko <vnbenko@gmail.com>
2021-05-16 19:05:24 +0300

    feat: implement the logic for method twoDimensionalSort for exercise-4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,31 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+    NSSortDescriptor* sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil
+                                                                     ascending:YES
+                                                                      selector:@selector(localizedCompare:)];
+    
+    NSArray *obtainedArray = [self obtainArray:array];
+    
+    if ([obtainedArray.firstObject isKindOfClass:[NSNumber class]]) {
+        return [obtainedArray sortedArrayUsingSelector:@selector(compare:)];
+        
+    } else if ([obtainedArray.firstObject isKindOfClass:[NSString class]]) {
+        return [obtainedArray sortedArrayUsingDescriptors:@[sortDescriptor]];
+    }
+    return @[];    
 }
+
+- (NSArray *) obtainArray:(NSArray *)array {
+    NSMutableArray *newArray = [NSMutableArray new];
+    
+    for (NSArray *subarray in array) {
+        if ([subarray isKindOfClass:[NSArray class]]) {
+            [newArray addObjectsFromArray:subarray];
+        }
+    }
+    return newArray;
+}
+
 
 @end
