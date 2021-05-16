d1b9d51c54bcfa4cadacf1b30c933da0a031d1a3
Vladimir Benko <vnbenko@gmail.com>
2021-05-16 17:19:11 +0300

    feat: implement the logic for exercise-1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,12 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 result = 0;
+    for (NSUInteger i = 0; i < 8; i++) {
+        if (n % 2) {
+            result += pow(2, 7 - i);
+        }
+        n /= 2;
+    }
+    return result;
 }
