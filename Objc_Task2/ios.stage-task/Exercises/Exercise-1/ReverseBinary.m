827a22e0bff59a4c57d54bec7372979462df2f96
Vladimir Benko <vnbenko@gmail.com>
2021-05-16 19:22:38 +0300

    fix: implement correct logic for ReverseInteger for exercise-1

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -2,11 +2,11 @@
 
 UInt8 ReverseInteger(UInt8 n) {
     UInt8 result = 0;
-    for (NSUInteger i = 0; i < 8; i++) {
-        if (n % 2) {
-            result += pow(2, 7 - i);
-        }
-        n /= 2;
+    
+    for (int i = 0; i < 8; i++) {
+        result = (result << 1) | (n & 1);
+        n >>= 1;
     }
+    
     return result;
 }
