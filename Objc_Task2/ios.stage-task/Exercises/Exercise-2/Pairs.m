431846a0aea476017477059405cee790ad54479c
Vladimir Benko <vnbenko@gmail.com>
2021-05-16 17:20:51 +0300

    feat: implement the method countPairs for exercise-2

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,14 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    int count = 0;
+    for (int i = 0; i < array.count; i++)
+        for (int j = i + 1; j < array.count; j++) {
+            if (ABS(array[i].intValue - array[j].intValue) == number.intValue) {
+                count++;
+            }
+        }
+    return count;
 }
 
 @end
