e69f4cc5f755dc138d98ad50612f493a49ef6a71
Vladimir Benko <vnbenko@gmail.com>
2021-05-16 22:04:24 +0300

    fix: change the logic of the method twoDimensionalSort

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,31 +3,38 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    NSSortDescriptor* sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:nil
-                                                                     ascending:YES
-                                                                      selector:@selector(localizedCompare:)];
-    
-    NSArray *obtainedArray = [self obtainArray:array];
-    
-    if ([obtainedArray.firstObject isKindOfClass:[NSNumber class]]) {
-        return [obtainedArray sortedArrayUsingSelector:@selector(compare:)];
-        
-    } else if ([obtainedArray.firstObject isKindOfClass:[NSString class]]) {
-        return [obtainedArray sortedArrayUsingDescriptors:@[sortDescriptor]];
+    if (array == nil || array.count == 0 || ![array[0] isKindOfClass:NSArray.class]){
+        return @[];
     }
-    return @[];    
+    
+    NSMutableArray *numbersArray = [NSMutableArray new];
+    NSMutableArray *stringsArray = [NSMutableArray new];
+    NSMutableArray *fullArray = [NSMutableArray new];
+    
+    for (NSArray *subArray in array) {
+            if ([subArray.firstObject isKindOfClass:NSNumber.class]) {
+                [numbersArray addObjectsFromArray:(subArray)];
+                [numbersArray sortUsingSelector:@selector(compare:)];
+            } else if ([subArray.firstObject isKindOfClass:NSString.class]) {
+                [stringsArray addObjectsFromArray:(subArray)];
+                [stringsArray sortUsingSelector:@selector(compare:)];
+            }
+    }
+    
+    if (numbersArray.count == 0) {
+        [fullArray addObjectsFromArray:stringsArray];
+    } else if (stringsArray.count == 0) {
+        [fullArray addObjectsFromArray:numbersArray];
+    } else {
+        [fullArray addObject:numbersArray];
+        [fullArray addObject:[stringsArray reverseObjectEnumerator].allObjects];
+    }
+
+    return fullArray;
 }
 
-- (NSArray *) obtainArray:(NSArray *)array {
-    NSMutableArray *newArray = [NSMutableArray new];
+
     
-    for (NSArray *subarray in array) {
-        if ([subarray isKindOfClass:[NSArray class]]) {
-            [newArray addObjectsFromArray:subarray];
-        }
-    }
-    return newArray;
-}
 
 
 @end
